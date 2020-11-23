36577f8d59aa340ed286b347544126a6b38e04e9
David Smith <dsmith@nerdwallet.com>
2020-11-23 02:04:08 -0800

    day3 parts 1 and 2 with gonum kdtree

--- day3/day3.go
+++ day3/day3.go
@@ -1,40 +1,251 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"strconv"
+
+	pkgmath "github.com/pkg/math"
+	"gonum.org/v1/gonum/spatial/kdtree"
 
 	"github.com/dds/aoc2020/util"
 )
 
-var Input = util.ParseInput(util.Inputs[3], util.CSVParser)[0]
+var Input = util.ParseInput(util.Inputs[3], util.CSVParser)
+
+func main() {
+	path1, path2 := Input[0], Input[1]
+	fmt.Println("part1", Cross(path1, path2))
+	fmt.Println("part2", MinWalk(path1, path2))
+}
+
+func MinWalk(path1 []string, path2 []string) int {
+	points := make(Points, 0)
+	var x, y, step int = 0, 0, 1
+	for _, i := range path1 {
+		dir, paces := Parse(i)
+		for _, p := range Walk(x, y, paces, dir) {
+			p.steps = step
+			step += 1
+			points = append(points, p)
+			x, y = p.x, p.y
+		}
+	}
+	tree := kdtree.New(points, true)
+	x, y, step = 0, 0, 1
+	crosses := make([][2]Point, 0)
+	for _, j := range path2 {
+		dir, paces := Parse(j)
+		for _, p := range Walk(x, y, paces, dir) {
+			p.steps = step
+			step += 1
+			x, y = p.x, p.y
+			q, d := tree.Nearest(p)
+			if d == 0 {
+				crosses = append(crosses, [2]Point{q.(Point), p})
+			}
+		}
+	}
+	if len(crosses) == 0 {
+		return 0
+	}
+	minstep := crosses[0][0].steps + crosses[0][1].steps
+	for _, t := range crosses[1:] {
+		s := t[0].steps + t[1].steps
+		if s < minstep {
+			minstep = s
+		}
+	}
+	return minstep
+}
 
 func Parse(pos string) (rune, int) {
 	dir := pos[0]
-	steps, err := strconv.Atoi(pos[1:])
+	paces, err := strconv.Atoi(pos[1:])
 	if err != nil {
 		panic(err)
 	}
-	return rune(dir), steps
+	return rune(dir), paces
+}
+
+func Walk(x, y, paces int, dir rune) (r []Point) {
+	var p, q int
+	switch dir {
+	case 'U':
+		q = 1
+	case 'D':
+		q = -1
+	case 'L':
+		p = -1
+	case 'R':
+		p = 1
+	}
+	for i := 0; i < paces; i++ {
+		pt := Point{x: x + p, y: y + q}
+		x, y = pt.x, pt.y
+		r = append(r, pt)
+	}
+	return
+}
+
+func Graph(tree *kdtree.Tree) string {
+	fmt.Println(tree.Root, tree.Root)
+	min, max := tree.Root.Min.(Point), tree.Root.Max.(Point)
+	fmt.Println("min, max", min, max)
+	width := max.x - min.x + 1
+	height := max.y - min.y + 1
+	fmt.Println("width, height", width, height)
+	cells := make([][]byte, height)
+	for i := 0; i < height; i++ {
+		cells[i] = make([]byte, width)
+		for j := 0; j < width; j++ {
+			cells[i][j] = '.'
+		}
+	}
+	f := func(c kdtree.Comparable, _ *kdtree.Bounding, _ int) bool {
+		p := c.(Point)
+		y := height - 1*(p.y-min.y) - 1
+		x := p.x - min.x
+		if y < 0 || x < 0 {
+			panic(fmt.Errorf("p=%v, x=%v, y=%v", p, x, y))
+		}
+		cells[y][x] = '#'
+		return false
+	}
+	tree.Do(f)
+	return string(bytes.Join(cells, []byte("\n")))
 }
 
 func Cross(path1 []string, path2 []string) int {
-	g := &util.Grid{}
+	points := make(Points, 0)
 	var x, y int
 	for _, i := range path1 {
-		dir, steps := Parse(i)
-		x, y = g.Translate(g.Walk(x, y, steps, dir))
-		g = g.AddStrip(x, y, steps, dir, "-")
+		dir, paces := Parse(i)
+		points = append(points, Walk(x, y, paces, dir)...)
+		x, y = int(points[len(points)-1].x), int(points[len(points)-1].y)
 	}
-	for _, i := range path2 {
-		dir, steps := Parse(i)
-		x, y = g.Translate(g.Walk(x, y, steps, dir))
-		g = g.AddStrip(x, y, steps, dir, "-")
+	tree := kdtree.New(points, true)
+	x, y = 0, 0
+	crosses := make([]Point, 0)
+	for _, j := range path2 {
+		dir, paces := Parse(j)
+		for _, p := range Walk(x, y, paces, dir) {
+			x, y = p.x, p.y
+			q, d := tree.Nearest(p)
+			if d == 0 {
+				crosses = append(crosses, q.(Point))
+			}
+		}
 	}
-	fmt.Println(g)
-	return 0
+	if len(crosses) == 0 {
+		return 0
+	}
+	min := manhattan(0, 0, crosses[0].x, crosses[0].y)
+	for _, t := range crosses {
+		d := manhattan(0, 0, t.x, t.y)
+		if d < min {
+			min = d
+		}
+	}
+	return min
 }
 
-func main() {
+func manhattan(x1, y1, x2, y2 int) int {
+	var p1, p2 int
+	if x2 > x1 {
+		p1 = x2 - x1
+	} else {
+		p1 = x1 - x2
+	}
+	if y2 > y1 {
+		p2 = y2 - y1
+	} else {
+		p2 = y1 - y2
+	}
+	return p1 + p2
+}
 
+type Point struct {
+	x, y  int
+	steps int
+}
+
+func (p Point) Compare(c kdtree.Comparable, d kdtree.Dim) float64 {
+	q := c.(Point)
+	switch d {
+	case 0:
+		return float64(p.x - q.x)
+	case 1:
+		return float64(p.y - q.y)
+	default:
+		panic("extra dimension")
+	}
+}
+
+func (p Point) Dims() int { return 2 }
+
+func (p Point) Distance(c kdtree.Comparable) float64 {
+	q := c.(Point)
+	return float64(manhattan(q.x, q.y, p.x, p.y))
+}
+
+func (p Point) Extend(b *kdtree.Bounding) *kdtree.Bounding {
+	if b == nil {
+		b = &kdtree.Bounding{Point{x: p.x, y: p.y}, Point{x: p.x, y: p.y}}
+	}
+	min := b.Min.(Point)
+	max := b.Max.(Point)
+	min.x = pkgmath.MinInt(min.x, p.x)
+	min.y = pkgmath.MinInt(min.y, p.y)
+	max.x = pkgmath.MaxInt(max.x, p.x)
+	max.y = pkgmath.MaxInt(max.y, p.y)
+	*b = kdtree.Bounding{Min: min, Max: max}
+	return b
+
+}
+
+type Points []Point
+
+func (p Points) Bounds() *kdtree.Bounding {
+	if len(p) == 0 {
+		return nil
+	}
+	min, max := Point{x: p[0].x, y: p[0].y}, Point{x: p[0].x, y: p[0].y}
+	for _, p := range p[1:] {
+		min.x = pkgmath.MinInt(min.x, p.x)
+		min.y = pkgmath.MinInt(min.y, p.y)
+		max.x = pkgmath.MaxInt(max.x, p.x)
+		max.y = pkgmath.MaxInt(max.y, p.y)
+	}
+	return &kdtree.Bounding{Min: min, Max: max}
+}
+
+func (p Points) Index(i int) kdtree.Comparable         { return p[i] }
+func (p Points) Len() int                              { return len(p) }
+func (p Points) Pivot(d kdtree.Dim) int                { return plane{Points: p, Dim: d}.Pivot() }
+func (p Points) Slice(start, end int) kdtree.Interface { return p[start:end] }
+
+// plane is required to help Points.
+type plane struct {
+	kdtree.Dim
+	Points
+}
+
+func (p plane) Less(i, j int) bool {
+	switch p.Dim {
+	case 0:
+		return p.Points[i].x < p.Points[j].x
+	case 1:
+		return p.Points[i].y < p.Points[j].y
+	default:
+		panic("illegal dimension")
+	}
+}
+func (p plane) Pivot() int { return kdtree.Partition(p, kdtree.MedianOfMedians(p)) }
+func (p plane) Slice(start, end int) kdtree.SortSlicer {
+	p.Points = p.Points[start:end]
+	return p
+}
+func (p plane) Swap(i, j int) {
+	p.Points[i], p.Points[j] = p.Points[j], p.Points[i]
 }
--- day3/day3_test.go
+++ day3/day3_test.go
@@ -8,7 +8,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func Test(t *testing.T) {
+func TestPart1(t *testing.T) {
 	type test struct {
 		input  string
 		expect int
@@ -36,6 +36,33 @@ U98,R91,D20,R16,D67,R40,U7,R15,U6,R7`,
 			path1 := input[0]
 			path2 := input[1]
 			require.Equal(t, test.expect, Cross(path1, path2))
+		})
+	}
+}
+
+func TestPart2(t *testing.T) {
+	type test struct {
+		input  string
+		expect int
+	}
+	tests := []test{
+		test{
+			input: `R75,D30,R83,U83,L12,D49,R71,U7,L72
+U62,R66,U55,R34,D71,R55,D58,R83`,
+			expect: 610,
+		},
+		test{
+			input: `R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51
+U98,R91,D20,R16,D67,R40,U7,R15,U6,R7`,
+			expect: 410,
+		},
+	}
+	for i, test := range tests {
+		t.Run(fmt.Sprint(i), func(t *testing.T) {
+			input := util.ParseInput(test.input, util.CSVParser)
+			path1 := input[0]
+			path2 := input[1]
+			require.Equal(t, test.expect, MinWalk(path1, path2))
 		})
 	}
 }
