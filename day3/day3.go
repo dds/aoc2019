17fb88c9d512feed15edd48741b33159d4eac24f
David Smith <dsmith@nerdwallet.com>
2020-11-22 17:31:00 -0800

    input for day3 and scaffold

---
+++ day3/day3.go
@@ -0,0 +1,13 @@
+package main
+
+import "github.com/dds/aoc2020/util"
+
+var Input = util.ParseInput(util.Inputs[3], util.CSVParser)[0]
+
+func Cross(path1 []string, path2 []string) int {
+	return 0
+}
+
+func main() {
+
+}
