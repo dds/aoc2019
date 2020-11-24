969b8e94c6b9457c82661c89fcbbfc537df98061
David Smith <dsmith@nerdwallet.com>
2020-11-24 01:05:58 -0800

    day8 boilerplate

---
+++ day8/day8.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/dds/aoc2020/util"
+)
+
+var Input = util.Inputs[0]
+
+func main() {
+	fmt.Println(part1(Input))
+}
+
+func part1(input string) (r string) {
+	return
+}
