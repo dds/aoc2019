cfa6592901f0ef043830145697eef19d64cd6deb
David Smith <dsmith@nerdwallet.com>
2020-11-23 17:45:28 -0800

    day7 boilerplate

---
+++ day7/day7.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/dds/aoc2020/util"
+)
+
+var Input = util.InputInts(util.Inputs[7], util.CSVParser)[0]
+
+func main() {
+	fmt.Println(part1(Input))
+	fmt.Println(part2(Input))
+}
+
+func part1(input []int) (r string) {
+	return
+}
+func part2(input []int) (r string) {
+	return
+}
