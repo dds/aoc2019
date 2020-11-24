130dd5a0d3d044d53b9ec5b7ab230d7edbe08a5b
David Smith <dsmith@nerdwallet.com>
2020-11-24 10:41:09 -0800

    day10 scaffolding

---
+++ day10/day10.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/dds/aoc2020/util"
+)
+
+var Input = util.Inputs[10]
+
+func main() {
+	fmt.Println(part1(Input))
+}
+
+func part1(input string) interface{} {
+	return input
+}
