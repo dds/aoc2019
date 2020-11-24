cf6290840a761660ec135cef514248fedd30fa00
David Smith <dsmith@nerdwallet.com>
2020-11-24 09:32:30 -0800

    add first test of part2

--- intcode/intcode_test.go
+++ intcode/intcode_test.go
@@ -94,6 +94,13 @@ func TestIO(t *testing.T) {
 			},
 			expect: []int{1000},
 		},
+		"big memory": test{
+			input: input{
+				code: []int{109, 1, 204, -1, 1001, 100, 1, 100, 1008, 100, 16, 101, 1006, 101, 0, 99},
+				in:   []int{},
+			},
+			expect: []int{109, 1, 204, -1, 1001, 100, 1, 100, 1008, 100, 16, 101, 1006, 101, 0, 99},
+		},
 	}
 
 	for i, test := range tests {
