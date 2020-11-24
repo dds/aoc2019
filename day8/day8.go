37d1d807ee27fcfaccf9925b0cd96100da856939
David Smith <dsmith@nerdwallet.com>
2020-11-24 01:10:42 -0800

    more day8

--- day8/day8.go
+++ day8/day8.go
@@ -6,12 +6,12 @@ import (
 	"github.com/dds/aoc2020/util"
 )
 
-var Input = util.Inputs[0]
+var Input = util.Inputs[8]
 
 func main() {
 	fmt.Println(part1(Input))
 }
 
 func part1(input string) (r string) {
-	return
+	return input
 }
