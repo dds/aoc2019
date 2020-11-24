149d25293012e5201cdbce2692333c19e69a30ff
David Smith <dsmith@nerdwallet.com>
2020-11-23 17:41:34 -0800

    day6 parts 1 and 2

--- day6/day6.go
+++ day6/day6.go
@@ -2,16 +2,79 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/dds/aoc2020/util"
 )
 
-var Input = util.Inputs[6]
+const (
+	COM = "COM"
+	YOU = "YOU"
+	SAN = "SAN"
+)
 
-func main() {
-	fmt.Println(part1(Input))
+var Input = util.ParseInput(util.Inputs[6], ParseInput)
+
+func ParseInput(input string) []string {
+	return util.TrimSpace(strings.FieldsFunc(input, func(c rune) bool { return c == ')' }))
 }
 
-func part1(input string) (r string) {
+func main() {
+	fmt.Println(Part1(Input))
+	fmt.Println(Part2(Input))
+}
+
+func Part1(input [][]string) (r int) {
+	return CountOrbits(Orbits(input))
+}
+
+func CountOrbits(o map[string]string) (r int) {
+	for _, v := range o {
+		next := v
+		for next != COM {
+			r++
+			next = o[next]
+		}
+	}
+	r += len(o)
 	return
+}
+
+func Orbits(input [][]string) map[string]string {
+	r := map[string]string{}
+	for _, fields := range input {
+		r[fields[1]] = fields[0]
+	}
+	return r
+}
+
+func Part2(input [][]string) (r int) {
+	return CountTransfers(Orbits(input))
+}
+
+func CountTransfers(o map[string]string) int {
+	var (
+		YourPath, SantasPath []string
+		next                 = o[YOU]
+	)
+
+	for next != "" {
+		YourPath = append(YourPath, o[next])
+		next = o[next]
+	}
+
+	next = o[SAN]
+	for next != "" {
+		SantasPath = append(SantasPath, o[next])
+		next = o[next]
+	}
+
+	for i := 0; i < len(YourPath); i++ {
+		for j := 0; j < len(SantasPath); j++ {
+			if YourPath[i] == SantasPath[j] {
+				return i + j + 2
+			}
+		}
+	}
+	return 0
 }
--- day6/day6_test.go
+++ day6/day6_test.go
@@ -4,24 +4,96 @@ import (
 	"fmt"
 	"testing"
 
+	"github.com/dds/aoc2020/util"
 	"github.com/stretchr/testify/require"
 )
 
-func Test(t *testing.T) {
+func TestOrbits(t *testing.T) {
+	type result struct {
+		m map[string]string
+		n int
+		t int
+	}
 	type test struct {
-		input  int
-		expect int
+		input  [][]string
+		expect result
 	}
 
 	tests := []test{
 		test{
-			// ...
+			input: util.ParseInput(`COM)B
+B)C
+C)D
+D)E
+E)F
+B)G
+G)H
+D)I
+E)J
+J)K
+K)L
+`, ParseInput),
+			expect: result{
+				n: 42,
+				m: map[string]string{
+					"B": "COM",
+					"G": "B",
+					"H": "G",
+					"C": "B",
+					"D": "C",
+					"I": "D",
+					"E": "D",
+					"J": "E",
+					"K": "J",
+					"L": "K",
+					"F": "E",
+				},
+			},
+		},
+		test{
+			input: util.ParseInput(`COM)B
+B)C
+C)D
+D)E
+E)F
+B)G
+G)H
+D)I
+E)J
+J)K
+K)L
+K)YOU
+I)SAN`, ParseInput),
+			expect: result{
+				n: 54,
+				m: map[string]string{
+					"B":   "COM",
+					"G":   "B",
+					"H":   "G",
+					"C":   "B",
+					"D":   "C",
+					"I":   "D",
+					"SAN": "I",
+					"E":   "D",
+					"J":   "E",
+					"K":   "J",
+					"YOU": "K",
+					"L":   "K",
+					"F":   "E",
+				},
+				t: 4,
+			},
 		},
 	}
 
 	for i, test := range tests {
 		t.Run(fmt.Sprint(i), func(t *testing.T) {
-			require.Equal(t, test.expect, test.input)
+			o := Orbits(test.input)
+			require.Equal(t, test.expect.m, o)
+			require.Equal(t, test.expect.n, CountOrbits(o))
+			if o["YOU"] != "" && o["SAN"] != "" {
+				require.Equal(t, test.expect.t, CountTransfers(o))
+			}
 		})
 	}
 }
