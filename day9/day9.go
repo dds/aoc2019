4cba0394480c1fb82902d9ac3b6cee72ef11094b
David Smith <dsmith@nerdwallet.com>
2020-11-24 09:24:51 -0800

    day9 boilerlate

---
+++ day9/day9.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/dds/aoc2020/intcode"
+	"github.com/dds/aoc2020/util"
+)
+
+var Input = util.InputInts(util.Inputs[9], util.CSVParser)[0]
+
+func main() {
+	ctx := util.ContextWithSignals(context.Background())
+	fmt.Println(part1(ctx, Input))
+	fmt.Println(part2(ctx, Input))
+}
+
+func part1(ctx context.Context, input []int) (r interface{}) {
+	out := make(chan int)
+	go intcode.Code(input).Exec(ctx, nil, out)
+	s := []int{}
+	for i := range out {
+		s = append(s, i)
+	}
+	return fmt.Sprint(s)
+}
+
+func part2(ctx context.Context, input []int) (r interface{}) {
+	return input
+}
