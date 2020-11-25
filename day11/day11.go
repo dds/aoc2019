3b431a3ee5767aa3a8440d67ad295608bb456ba2
David Smith <dsmith@nerdwallet.com>
2020-11-24 18:05:37 -0800

    start day11

--- day11/day11.go
+++ day11/day11.go
@@ -13,14 +13,24 @@ var Input = util.InputInts(util.Inputs[11], util.CSVParser)[0]
 func main() {
 	ctx := util.ContextWithSignals(context.Background())
 	fmt.Println(part1(ctx, Input))
-	fmt.Println(part2(ctx, Input))
+	// fmt.Println(part2(ctx, Input))
+}
+
+type out struct {
+	color, dir int
 }
 
 func part1(ctx context.Context, input []int) (r interface{}) {
 	out := make(chan int)
 	in := make(chan int)
 	go intcode.Code(input).Exec(ctx, in, out)
-	in <- 1
+
+	go func() {
+		for i := 0; i < 10; i++ {
+			in <- 0
+		}
+		close(in)
+	}()
 	s := []int{}
 	for i := range out {
 		s = append(s, i)
