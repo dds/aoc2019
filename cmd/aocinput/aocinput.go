dds/aoc2020

rename util to lib

cmd/aocinput/aocinput.go
@@ -6,7 +6,7 @@ import (
 
 	"github.com/alecthomas/kong"
 	"github.com/atotto/clipboard"
-	"github.com/dds/aoc2020/util"
+	"github.com/dds/aoc2020/lib"
 )
 
 var CLI struct {
@@ -19,10 +19,10 @@ var CLI struct {
 func main() {
 	kong.Parse(&CLI)
 	deadline := time.Now().Add(CLI.Timeout)
-	s, err := util.GetInput(CLI.Day, CLI.Session)
+	s, err := lib.GetInput(CLI.Day, CLI.Session)
 	for s == "" && time.Now().Before(deadline) {
 		time.Sleep(3 * time.Second)
-		s, err = util.GetInput(CLI.Day, CLI.Session)
+		s, err = lib.GetInput(CLI.Day, CLI.Session)
 	}
 	if err != nil {
 		panic(err)

example_test.go
@@ -3,11 +3,11 @@ package main
 import (
 	"fmt"
 
-	"github.com/dds/aoc2020/util"
+	"github.com/dds/aoc2020/lib"
 )
 
 func Example() {
-	input := util.InputInts(util.Inputs[0], func(s string) []string { return util.TrimSpace(util.CSVParser(s)) })
+	input := lib.InputInts(lib.Inputs[0], lib.NumberParser)
 	for i, l := range input {
 		fmt.Println(i, l)
 	}

lib/inputs.go
@@ -1,4 +1,4 @@
-package util
+package lib
 
 import (
 	"fmt"
@@ -7,7 +7,7 @@ import (
 	"strconv"
 	"strings"
 
-	"github.com/dds/aoc2020/util/inputs"
+	"github.com/dds/aoc2020/lib/inputs"
 )
 
 // All puzzle inputs stored as an array of UTF-8 strings.

scaffold/dayN.go
@@ -4,10 +4,10 @@ import (
 	"fmt"
 	"testing"
 
-	"github.com/dds/aoc2020/util"
+	"github.com/dds/aoc2020/lib"
 )
 
-var Input = util.Inputs[0]
+var Input = lib.Inputs[0]
 
 func Test(t *testing.T) {
 	// type test struct {

dds/aoc2019

cmd/aocinput/aocinput.go
@@ -6,7 +6,7 @@ import (
 
 	"github.com/alecthomas/kong"
 	"github.com/atotto/clipboard"
-	"github.com/dds/aoc2019/util"
+	"github.com/dds/aoc2019/lib"
 )
 
 var CLI struct {
@@ -19,10 +19,10 @@ var CLI struct {
 func main() {
 	kong.Parse(&CLI)
 	deadline := time.Now().Add(CLI.Timeout)
-	s, err := util.GetInput(CLI.Day, CLI.Session)
+	s, err := lib.GetInput(CLI.Day, CLI.Session)
 	for s == "" && time.Now().Before(deadline) {
 		time.Sleep(3 * time.Second)
-		s, err = util.GetInput(CLI.Day, CLI.Session)
+		s, err = lib.GetInput(CLI.Day, CLI.Session)
 	}
 	if err != nil {
 		panic(err)

example_test.go
@@ -3,11 +3,11 @@ package main
 import (
 	"fmt"
 
-	"github.com/dds/aoc2019/util"
+	"github.com/dds/aoc2019/lib"
 )
 
 func Example() {
-	input := util.InputInts(util.Inputs[0], func(s string) []string { return util.TrimSpace(util.CSVParser(s)) })
+	input := lib.InputInts(lib.Inputs[0], lib.NumberParser)
 	for i, l := range input {
 		fmt.Println(i, l)
 	}

lib/inputs.go
@@ -1,4 +1,4 @@
-package util
+package lib
 
 import (
 	"fmt"
@@ -7,7 +7,7 @@ import (
 	"strconv"
 	"strings"
 
-	"github.com/dds/aoc2019/util/inputs"
+	"github.com/dds/aoc2019/lib/inputs"
 )
 
 // All puzzle inputs stored as an array of UTF-8 strings.

scaffold/dayN.go
@@ -4,10 +4,10 @@ import (
 	"fmt"
 	"testing"
 
-	"github.com/dds/aoc2019/util"
+	"github.com/dds/aoc2019/lib"
 )
 
-var Input = util.Inputs[0]
+var Input = lib.Inputs[0]
 
 func Test(t *testing.T) {
 	// type test struct {