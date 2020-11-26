52d021b247873d8c2a0871546a4308cd18c3ed76
David D. Smith <dsmith@nerdwallet.com>
2020-11-25 23:31:21 -0800

    feat: day13 p2

--- day13/day13.go
+++ day13/day13.go
@@ -3,10 +3,12 @@ package main
 import (
 	"context"
 	"fmt"
+	"os"
 	"time"
 
 	"github.com/dds/aoc2019/intcode"
 	"github.com/dds/aoc2019/lib"
+	"github.com/gdamore/tcell"
 )
 
 var Input = lib.InputInts(lib.Inputs[13], lib.CSVParser)[0]
@@ -20,12 +22,12 @@ func main() {
 type shape int
 
 type rec struct {
-	x, y int
-	id   shape
+	x, y  int
+	shape shape
 }
 
 func (r rec) String() string {
-	return fmt.Sprintf("(%d, %d): %q", r.x, r.y, r.id)
+	return fmt.Sprintf("(%d, %d): %v", r.x, r.y, r.shape)
 }
 
 const (
@@ -36,16 +38,16 @@ const (
 	ball
 )
 
-var shapes = map[shape]string{
-	empty:  " ",
-	wall:   "=",
-	block:  "#",
-	paddle: "|",
-	ball:   "o",
+var shapes = map[shape]rune{
+	empty:  ' ',
+	wall:   '|',
+	block:  '#',
+	paddle: '-',
+	ball:   'o',
 }
 
 func (s shape) String() string {
-	return shapes[s]
+	return string(shapes[s])
 }
 
 func recs(ctx context.Context, i <-chan int) <-chan rec {
@@ -79,7 +81,7 @@ func recs(ctx context.Context, i <-chan int) <-chan rec {
 				if !ok {
 					return
 				}
-				r.id = shape(t)
+				r.shape = shape(t)
 			}
 			select {
 			case <-ctx.Done():
@@ -103,7 +105,7 @@ func part1(ctx context.Context, input []int) (r int) {
 	}()
 	var i = 0
 	for r := range recs(ctx, out) {
-		if r.id != block {
+		if r.shape != block {
 			continue
 		}
 		i++
@@ -111,28 +113,141 @@ func part1(ctx context.Context, input []int) (r int) {
 	return i
 }
 
-func part2(ctx context.Context, input []int) (r int) {
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+type point struct {
+	x, y int
+}
+
+func part2(ctx context.Context, input []int) (rc int) {
+	input[0] = 2
+	// Boot intcode computer.
 	in := make(chan int)
 	out := make(chan int)
-	ctx, cancel := context.WithDeadline(ctx, time.Now().Add(3*time.Second))
-	defer cancel()
-	input[0] = 2
+	errs := make(chan error)
 	go func() {
 		if err := intcode.Code(input).Exec(ctx, in, out); err != nil {
-			panic(err)
+			errs <- err
 		}
 	}()
-	// go func() {
-	// 	for {
-	// 		select {
-	// 		case <-ctx.Done():
-	// 			return
-	// 		case in <- 1:
-	// 		}
-	// 	}
-	// }()
-	for i := range recs(ctx, out) {
-		fmt.Println(i)
+	recs := recs(ctx, out)
+
+	var err error
+
+	// Setup screen.
+	// encoding.Register()
+	// cb := &tcell.CellBuffer{}
+	// fmt.Println(cb)
+
+	var sc tcell.Screen
+	sc, err = tcell.NewScreen()
+	if err != nil {
+		panic(err)
 	}
-	return 0
+	if err := sc.Init(); err != nil {
+		panic(err)
+	}
+	style := tcell.StyleDefault.Background(tcell.ColorBlack).Foreground(tcell.ColorWhite)
+	sc.SetStyle(style)
+	// sc.Clear()
+
+	userQuit := make(chan struct{})
+	go func() {
+		for {
+			switch ev := sc.PollEvent().(type) {
+			case *tcell.EventResize:
+				sc.Sync()
+			case *tcell.EventKey:
+				if ev.Key() == tcell.KeyEscape {
+					userQuit <- struct{}{}
+				}
+			}
+		}
+	}()
+
+	// Start game loop.
+	var (
+		quit bool
+		p    point
+		w, h int
+	)
+	refreshRate := time.Second / 360
+	refresh := time.NewTimer(refreshRate)
+	const notpending = -2
+	var pending = notpending
+	var pendingC chan int
+	for !quit {
+		if pending != notpending {
+			// If we have a pending send, set its channel
+			pendingC = in
+		}
+		// Setup pending input to game.
+		select {
+		case err = <-errs:
+			quit = true
+		case <-ctx.Done():
+			// Quit if canceled.
+			quit = true
+		case <-userQuit:
+			quit = true
+		case pendingC <- pending:
+			// Send pending input to game.
+			pendingC = nil
+			pending = notpending
+		case u, ok := <-recs:
+			if !ok {
+				quit = true
+			}
+			w = max(w, u.x)
+			h = max(h, u.y)
+			switch u.shape {
+			case paddle:
+				p.x = u.x
+				p.y = u.y
+			case ball:
+				if p.x == u.x {
+					pending = 0
+				} else if p.x < u.x {
+					pending = 1
+				} else {
+					pending = -1
+				}
+			}
+			// Update screen based on output.
+			if u.x == -1 && u.y == 0 {
+				u.y = h + 1
+				u.x = (w / 2) - 2
+				rc = int(u.shape)
+			}
+			sc.SetContent(u.x, u.y, shapes[u.shape], nil, tcell.StyleDefault)
+			for i, c := range fmt.Sprint(rc) {
+				sc.SetContent(w/2+i, h+1, c, nil, tcell.StyleDefault)
+			}
+			// sc.SetContent(u.x, u.y, shapes[u.shape], nil, 0)
+		}
+		<-refresh.C
+		// Redraw on timer elapse.
+		refresh.Reset(refreshRate)
+		// Update score
+		// Draw board
+		sc.Show()
+	}
+	sc.Fini()
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+	}
+	return
 }
