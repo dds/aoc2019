d9bb31f5e544bef9acf7d65a0114521fa99c26e8
David D. Smith <dsmith@nerdwallet.com>
2020-11-29 12:35:10 -0800

    feat: day16 part1

--- day16/day16.go
+++ day16/day16.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"regexp"
+	"strconv"
 
 	"github.com/dds/aoc2019/lib"
 	"github.com/dds/aoc2019/lib/inputs"
@@ -21,11 +22,74 @@ func main() {
 	fmt.Println(part2(Input))
 }
 
-func part1(input []int) (rc int) {
-	fmt.Println(input)
+var base = []int{0, 1, 0, -1}
+
+func coefficients(n, row int) []int {
+	r := make([]int, n+1)
+	var i, digit int
+	for i < n+1 {
+		for j := 0; j < row+1 && i+j < n+1; j++ {
+			r[i+j] = base[digit]
+		}
+		i += row + 1
+		digit = (digit + 1) % 4
+	}
+	r = r[1:]
+	return r
+}
+
+func phase(input []int) (output []int) {
+	n := len(input)
+	output = make([]int, n)
+	for i := 0; i < n; i++ {
+		coef := coefficients(n, i)
+		var c int
+		for j := 0; j < len(coef); j++ {
+			c += input[j] * coef[j]
+		}
+		if c < 0 {
+			c = -c
+		}
+		output[i] = c % 10
+	}
 	return
 }
 
+func part1(input []int) (rc int) {
+	fmt.Println("Input len: ", len(input))
+	fmt.Println("Initial input: ", input)
+	fmt.Println("first coefficients: ", coefficients(len(input), 0))
+	fmt.Println("second coefficients: ", coefficients(len(input), 1))
+	phased := phase(input)
+	fmt.Println("First phase:", phased)
+
+	for i := 0; i < 99; i++ {
+		phased = phase(phased)
+	}
+	var join string
+	for _, i := range phased {
+		join += fmt.Sprint(i)
+	}
+	a, err := strconv.Atoi(join[:8])
+	if err != nil {
+		panic(err)
+	}
+	return a
+}
+
 func part2(input []int) (rc int) {
-	return
+	signal := phase(input)
+	for i := 0; i < 999; i++ {
+		signal = phase(signal)
+	}
+
+	var join string
+	for _, i := range signal {
+		join += fmt.Sprint(i)
+	}
+	a, err := strconv.Atoi(join[:8])
+	if err != nil {
+		panic(err)
+	}
+	return a
 }
