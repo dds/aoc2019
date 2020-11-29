7b33b012e525747c5e9741ac0e7f7bcf7ea1fbe8
David D. Smith <dsmith@nerdwallet.com>
2020-11-29 10:34:47 -0800

    test: day16 failing tests

--- day16/day16.go
+++ day16/day16.go
@@ -3,34 +3,18 @@ package main
 import (
 	"fmt"
 	"regexp"
-	"testing"
 
 	"github.com/dds/aoc2019/lib"
 	"github.com/dds/aoc2019/lib/inputs"
 )
 
-var Input = lib.InputInts(inputs.Day16(), func(input string) []string {
-	return regexp.MustCompile(`\d`).FindAllString(input, -1)
-})[0]
-
-func Test(t *testing.T) {
-	// type test struct {
-	// 	input  int
-	// 	expect int
-	// }
-
-	// tests := []test{
-	// 	test{
-	// 		// ...
-	// 	},
-	// }
-
-	// for i, test := range tests {
-	// 	t.Run(fmt.Sprint(i), func(t *testing.T) {
-	// 		require.Equal(t, test.expect, test.input)
-	// 	})
-	// }
+func parse(input string) []int {
+	return lib.InputInts(input, func(input string) []string {
+		return regexp.MustCompile(`\d`).FindAllString(input, -1)
+	})[0]
 }
+
+var Input = parse(inputs.Day16())
 
 func main() {
 	fmt.Println(part1(Input))
